7bf88118031c72957c79e4fd4b7f9473f56d6a15
amirs-web <siami.amir4@gmail.com>
2024-04-20 09:55:39 +0330

    first commit

---
+++ src/components/Options.jsx
@@ -0,0 +1,27 @@
+function Options({ question, answer, dispatch }) {
+  const hasAnswered = answer !== null;
+  return (
+    <div className="options">
+      {question.options.map((option, i) => {
+        return (
+          <button
+            className={`btn btn-option ${i === answer ? "answer" : ""} ${
+              hasAnswered
+                ? i === question.correctOption
+                  ? "correct"
+                  : "wrong"
+                : ""
+            }`}
+            key={option}
+            disabled={hasAnswered}
+            onClick={() => dispatch({ type: "newAnswer", payload: i })}
+          >
+            {option}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Options;
